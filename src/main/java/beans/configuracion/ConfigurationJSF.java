f4405ee22060e288c7c90834d16e33acadbc9d99
Sergio Villalba Díaz <sergiovillalba522@gmail.com>
2020-07-03 19:37:07 -0500

    Linea base de configuración JSF

---
+++ src/main/java/beans/configuracion/ConfigurationJSF.java
@@ -0,0 +1,12 @@
+package beans.configuracion;
+
+import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.FacesConfig.Version;
+import javax.enterprise.context.*;
+
+@FacesConfig( version = Version.JSF_2_3)
+@ApplicationScoped
+
+public class ConfigurationJSF {
+
+}
